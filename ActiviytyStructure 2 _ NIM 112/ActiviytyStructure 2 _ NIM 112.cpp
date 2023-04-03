1316e06780b6680904d5eead03dab34a2b4758a6
sidik30 <sidikistiqomah123@gmail.com>
2023-04-03 11:45:37 +0700

    masukan data

--- ActiviytyStructure 2 _ NIM 112/ActiviytyStructure 2 _ NIM 112.cpp
+++ ActiviytyStructure 2 _ NIM 112/ActiviytyStructure 2 _ NIM 112.cpp
@@ -16,5 +16,20 @@ struct Mahasiswa
 };
 
 
+int main() {
+	Mahasiswa mhs;
+
+	cout << "Masukan NIM :";
+	cin >> mhs.NIM;
+	cout << "Masukan nama :";
+	cin >> mhs.nama;
+	cout << "Alamat :";
+	cout << "\n\tMasukan Desa :";
+	cin >> mhs.alamat.desa;
+	cout << "\n\tMasukan Kota :";
+	cin >> mhs.alamat.kota;
+	cout << "Masukan Umur :";
+	cin >> mhs.umur;
+
 
 }
