012590272260468e46b9d79bdfbb1dae135fe314
sidik30 <sidikistiqomah123@gmail.com>
2023-04-03 10:57:45 +0700

    menampilkan

--- ActicityStructure_3NIM112/ActicityStructure_3NIM112.cpp
+++ ActicityStructure_3NIM112/ActicityStructure_3NIM112.cpp
@@ -13,9 +13,19 @@ int main() {
 	mahasiswa mhs1, mhs2;
 
 	mhs1.NIM = "20220140112";
-	mhs1.nama = "Ahmad sidik";
+	mhs1.nama = "Sidik";
 	mhs1.alamat = "Bengkulu";
 	mhs1.umur = 20;
 
-	cout<<
+	cout << "Mahasiswa NIM :";
+	cin >> mhs2.NIM;
+	cout << "Masukan Nama :";
+	cin >> mhs2.nama;
+	cout << "Masukan Alamat:";
+	cin >> mhs2.alamat;
+	cout << "Masukan Umur:";
+	cin >> mhs2.umur;
+
+
+
 }
